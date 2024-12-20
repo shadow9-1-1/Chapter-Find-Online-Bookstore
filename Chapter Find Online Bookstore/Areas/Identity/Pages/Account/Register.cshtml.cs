b67e7ba7c889bdd3d8895f0a705c699a2661f6c1
Ahmed Wael <ahmedwael2612004@gmail.com>
2024-12-20 03:45:43 +0200

    user page update befor add OAuth

--- Chapter Find Online Bookstore/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Chapter Find Online Bookstore/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -150,6 +150,9 @@ namespace Chapter_Find_Online_Bookstore.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
+                    //set user role
+                    await _userManager.AddToRoleAsync(user, "customers");
+
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
--- Chapter Find Online Bookstore/Pages/user.cshtml.cs
+++ Chapter Find Online Bookstore/Pages/user.cshtml.cs
@@ -1,12 +1,27 @@
+using Chapter_Find_Online_Bookstore.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chapter_Find_Online_Bookstore.Pages
 {
+    [Authorize]
     public class userModel : PageModel
     {
+        private readonly UserManager<ApplicationUser> userManager;
+        public ApplicationUser? appUser;
+        public userModel(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
         public void OnGet()
         {
+            var user = userManager.GetUserAsync(User);
+            user.Wait();
+            appUser = user.Result;
+            
         }
     }
 }
